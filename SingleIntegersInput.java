4b71e0a18511a6ee461a7d5bee21e8c9f4213172
G-Rajesh0512 <139709257+G-Rajesh0512@users.noreply.github.com>
2023-07-17 02:56:46 +0530

    Add files via upload

---
+++ SingleIntegersInput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SingleIntegersInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of x: ");
+        int x = scanner.nextInt();
+
+        System.out.print("Output: ");
+        for (int i = 1; i <= x; i++) {
+            int num = 2 * i - 1;
+            if (i != x) {
+                System.out.print(num + ", ");
+            } else {
+                System.out.print(num);
+                
+                scanner.close();
+            }
+        }
+    }
+}
